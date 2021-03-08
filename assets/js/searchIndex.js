daade6b4d08c1b05a7254df303c40b65108ef83b
GitHub <noreply@github.com>
2021-03-08 13:05:18 +0000

    Continuous Integration Publish: 6670452287095d2f06d831eacab65e6f115b1245 Merge pull request #45 from cake-contrib/dependabot/nuget/develop/Cake.Testing-1.1.0

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,14 +39,14 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"NpmModule",
-            content:"NpmModule",
+            title:"INpmContentResolver",
+            content:"INpmContentResolver",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.Npm.Module/api/Cake.Npm.Module/NpmModule',
-            title:"NpmModule",
+            url:'/Cake.Npm.Module/api/Cake.Npm.Module/INpmContentResolver',
+            title:"INpmContentResolver",
             description:""
         }
     );
@@ -67,34 +67,6 @@ var searchModule = function() {
     a(
         {
             id:2,
-            title:"NpmContentResolver",
-            content:"NpmContentResolver",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.Npm.Module/api/Cake.Npm.Module/NpmContentResolver',
-            title:"NpmContentResolver",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
-            title:"INpmContentResolver",
-            content:"INpmContentResolver",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.Npm.Module/api/Cake.Npm.Module/INpmContentResolver',
-            title:"INpmContentResolver",
-            description:""
-        }
-    );
-    a(
-        {
-            id:4,
             title:"Constants",
             content:"Constants",
             description:'',
@@ -103,6 +75,34 @@ var searchModule = function() {
         {
             url:'/Cake.Npm.Module/api/Cake.Npm.Module/Constants',
             title:"Constants",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
+            title:"NpmModule",
+            content:"NpmModule",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Npm.Module/api/Cake.Npm.Module/NpmModule',
+            title:"NpmModule",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
+            title:"NpmContentResolver",
+            content:"NpmContentResolver",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Npm.Module/api/Cake.Npm.Module/NpmContentResolver',
+            title:"NpmContentResolver",
             description:""
         }
     );
