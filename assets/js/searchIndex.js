bcdd847ca20079f8136ad2774df9e4eeca84a1e8
GitHub <noreply@github.com>
2022-04-15 20:41:21 +0000

    Continuous Integration Publish: 4747fbe031ca5d0d1e5ada9619f65bd52b67e6fe Merge pull request #100 from cake-contrib/dependabot/nuget/develop/Cake.Testing-2.2.0
    Bump Cake.Testing from 2.1.0 to 2.2.0

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,34 +39,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"Constants",
-            content:"Constants",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.Npm.Module/api/Cake.Npm.Module/Constants',
-            title:"Constants",
-            description:""
-        }
-    );
-    a(
-        {
-            id:1,
-            title:"ModulesInstallationLocation",
-            content:"ModulesInstallationLocation",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.Npm.Module/api/Cake.Npm.Module/ModulesInstallationLocation',
-            title:"ModulesInstallationLocation",
-            description:""
-        }
-    );
-    a(
-        {
-            id:2,
             title:"NpmModule",
             content:"NpmModule",
             description:'',
@@ -80,7 +52,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:3,
+            id:1,
             title:"NpmPackageInstaller",
             content:"NpmPackageInstaller",
             description:'',
@@ -89,6 +61,34 @@ var searchModule = function() {
         {
             url:'/Cake.Npm.Module/api/Cake.Npm.Module/NpmPackageInstaller',
             title:"NpmPackageInstaller",
+            description:""
+        }
+    );
+    a(
+        {
+            id:2,
+            title:"Constants",
+            content:"Constants",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Npm.Module/api/Cake.Npm.Module/Constants',
+            title:"Constants",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
+            title:"NpmContentResolver",
+            content:"NpmContentResolver",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Npm.Module/api/Cake.Npm.Module/NpmContentResolver',
+            title:"NpmContentResolver",
             description:""
         }
     );
@@ -109,14 +109,14 @@ var searchModule = function() {
     a(
         {
             id:5,
-            title:"NpmContentResolver",
-            content:"NpmContentResolver",
+            title:"ModulesInstallationLocation",
+            content:"ModulesInstallationLocation",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.Npm.Module/api/Cake.Npm.Module/NpmContentResolver',
-            title:"NpmContentResolver",
+            url:'/Cake.Npm.Module/api/Cake.Npm.Module/ModulesInstallationLocation',
+            title:"ModulesInstallationLocation",
             description:""
         }
     );
