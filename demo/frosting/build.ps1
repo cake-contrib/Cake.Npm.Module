77099aba25766d2f5c34dfdab3ae6c12a2b64b92
Nils Andresen <nils@nils-andresen.de>
2023-01-07 21:08:27 +0100

    (maint) use project reference in frosting demo

--- demo/frosting/build.ps1
+++ demo/frosting/build.ps1
@@ -1,14 +1,5 @@
 $ErrorActionPreference = 'Stop'
 
 Set-Location -LiteralPath $PSScriptRoot
-
-# make sure we always get a fresh nuget-package and nothing from cache!
-dotnet nuget locals all --clear
-
-if(Test-Path .\build\tools) {
-  rm -Force -Recurse .\build\tools
-}
-
-dotnet add .\build\Build.csproj package Cake.Npm.Module --prerelease
 dotnet run --project build\Build.csproj -- $args
 exit $LASTEXITCODE;
